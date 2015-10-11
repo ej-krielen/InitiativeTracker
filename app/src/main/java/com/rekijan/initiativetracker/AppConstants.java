35998483028ba06a881e674afc24be6e1779e1d0
Erik-Jan Krielen <ej-krielen@gmail.com>
2015-10-11 18:43:21 +0200

    Splash screen now goes to mainactivity (with fragment) after timer

---
+++ app/src/main/java/com/rekijan/initiativetracker/AppConstants.java
@@ -0,0 +1,14 @@
+package com.rekijan.initiativetracker;
+
+/**
+ * Constant fields
+ *
+ * @author Erik-Jan Krielen ej.krielen@gmail.com
+ * @since 11-10-2015
+ */
+public class AppConstants {
+
+    // Splash screen timer
+    public static final int SPLASH_TIME_OUT = 3000;
+
+}
---
+++ app/src/main/java/com/rekijan/initiativetracker/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.rekijan.initiativetracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.rekijan.initiativetracker.ui.activities.MainActivity;
+import static com.rekijan.initiativetracker.AppConstants.*;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //Show splash screen with a timer.
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                // Close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
+
