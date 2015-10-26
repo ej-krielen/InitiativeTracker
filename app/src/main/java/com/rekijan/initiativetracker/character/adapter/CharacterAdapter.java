519b8e3f1d487a5da76a6c22db35c16fd6c51e92
Erik-Jan Krielen <ej-krielen@gmail.com>
2015-10-26 22:16:47 +0100

    Refactor listener outside of adapter

--- app/src/main/java/com/rekijan/initiativetracker/character/adapter/CharacterAdapter.java
+++ app/src/main/java/com/rekijan/initiativetracker/character/adapter/CharacterAdapter.java
@@ -2,9 +2,6 @@ package com.rekijan.initiativetracker.character.adapter;
 
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
-import android.text.Editable;
-import android.text.TextUtils;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +9,7 @@ import android.widget.EditText;
 
 import com.rekijan.initiativetracker.R;
 import com.rekijan.initiativetracker.character.model.CharacterModel;
+import com.rekijan.initiativetracker.listeners.GenericTextWatcher;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -119,64 +117,6 @@ public class CharacterAdapter extends RecyclerView.Adapter<CharacterAdapter.Char
         holder.characterDebuffBC.addTextChangedListener(new GenericTextWatcher(character, holder.characterDebuffBC));
         holder.characterDebuffBR.addTextChangedListener(new GenericTextWatcher(character, holder.characterDebuffBR));
 
-    }
-
-    public class GenericTextWatcher implements TextWatcher {
-        private View view;
-        private CharacterModel character;
-
-        public GenericTextWatcher(CharacterModel character, View view) {
-            this.character = character;
-            this.view = view;
-
-        }
-
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-        }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-            String text = s.toString();
-            switch (view.getId()) {
-                case R.id.character_initiative_editText:
-                    character.setInitiative(TextUtils.isEmpty(text) ? 0 : Integer.parseInt(text));
-                    break;
-                case R.id.character_name_editText:
-                    character.setCharacterName(text);
-                    break;
-                case R.id.character_notes_editText:
-                    character.setCharacterNotes(text);
-                    break;
-                case R.id.character_hp_editText:
-                    character.setHp(TextUtils.isEmpty(text) ? 0 : Integer.parseInt(text));
-                    break;
-                case R.id.character_debuff_TL_editText:
-                    character.setDebuffTL(TextUtils.isEmpty(text) ? 0 : Integer.parseInt(text));
-                    break;
-                case R.id.character_debuff_TC_editText:
-                    character.setDebuffTC(TextUtils.isEmpty(text) ? 0 : Integer.parseInt(text));
-                    break;
-                case R.id.character_debuff_TR_editText:
-                    character.setDebuffTR(TextUtils.isEmpty(text) ? 0 : Integer.parseInt(text));
-                    break;
-                case R.id.character_debuff_BL_editText:
-                    character.setDebuffBL(TextUtils.isEmpty(text) ? 0 : Integer.parseInt(text));
-                    break;
-                case R.id.character_debuff_BC_editText:
-                    character.setDebuffBC(TextUtils.isEmpty(text) ? 0 : Integer.parseInt(text));
-                    break;
-                case R.id.character_debuff_BR_editText:
-                    character.setDebuffBR(TextUtils.isEmpty(text) ? 0 : Integer.parseInt(text));
-                    break;
-            }
-        }
     }
 
     @Override
